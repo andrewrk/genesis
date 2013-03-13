e1b6e9fab15a2ebedd52482a8507ffe97ede0b3c
Andrew Kelley <superjoe30@gmail.com>
2013-03-13 03:13:05 -0400

    non-working integration with pulsego

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Digital Audio Workstation
+
+## Roadmap
+
+ * sound synthesis
+ * integrate with http://www.freesound.org/docs/api/resources.html
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/mattn/go-gtk/gtk"
+	"github.com/superjoe30/pulsego"
 	"os"
 )
 
@@ -15,4 +17,51 @@ func main() {
 	window.ShowAll()
 
 	gtk.Main()
+
+	pa := pulse.NewPulseMainLoop()
+	defer pa.Dispose()
+	pa.Start()
+
+	sync_ch := make(chan int)
+	go myMain(sync_ch, pa)
+	<-sync_ch
+}
+
+func send(ch chan int, val int) {
+	ch <- val
+}
+
+func myMain(sync_ch chan int, pa *pulse.PulseMainLoop) {
+	defer send(sync_ch, 0)
+	ctx := pa.NewContext("default", 0)
+	if ctx == nil {
+		fmt.Println("Failed to create a new context")
+		return
+	}
+	defer ctx.Dispose()
+	st := ctx.NewStream("default", &pulse.PulseSampleSpec{
+		Format: pulse.SAMPLE_FLOAT32LE, Rate: 22500, Channels: 1})
+	if st == nil {
+		fmt.Println("Failed to create a new stream")
+		return
+	}
+	defer st.Dispose()
+	st.ConnectToSink()
+	var samples [4096]float32
+	const period = 40
+	count := 0
+	for {
+		for i := range samples {
+			if count < period/2 {
+				samples[i] = -0.8
+			} else {
+				samples[i] = 0.8
+			}
+			count += 1
+			if count >= period {
+				count = 0
+			}
+		}
+		st.Write(samples, pulse.SEEK_RELATIVE)
+	}
 }
