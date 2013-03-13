d7af744b8421607caef5c4c6c8820f9b82c6974c
Andrew Kelley <superjoe30@gmail.com>
2013-03-13 02:17:52 -0400

    hello gtk

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/mattn/go-gtk/gtk"
+	"os"
+)
 
 func main() {
-    fmt.Printf("Hello, world\n")
+	gtk.Init(&os.Args)
+	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	window.SetTitle("Hello world")
+	window.Connect("destroy", gtk.MainQuit)
+
+	window.SetSizeRequest(400, 400)
+	window.ShowAll()
+
+	gtk.Main()
 }
