fe6fee9a5899d5882898c4fa7866e6a5e5c6a519
Andrew Kelley <superjoe30@gmail.com>
2015-01-19 10:20:20 -0700

    fix compilation errors with newer rustc

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,14 @@ extern crate glfw;
 use glfw::{Context, OpenGlProfileHint, WindowHint, WindowMode};
 
 fn main() {
-    let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
     glfw.window_hint(WindowHint::ContextVersion(3, 2));
     glfw.window_hint(WindowHint::OpenglForwardCompat(true));
     glfw.window_hint(WindowHint::OpenglProfile(OpenGlProfileHint::Core));
 
-    let (window, _) = glfw.create_window(800, 600, "genesis", WindowMode::Windowed)
+    let (mut window, _) = glfw.create_window(800, 600, "genesis", WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
-
-    // must make context current before calling gl::load_with
-    window.make_current();
 
     gl::load_with(|s| window.get_proc_address(s));
 
