4f8bc9fe7d4b4e000fdcc0186c7ceff35920909b
Andrew Kelley <superjoe30@gmail.com>
2015-01-21 12:30:17 -0700

    loading a file in another thread

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+#![allow(unstable)]
+
 extern crate gfx;
 extern crate glutin;
+extern crate groove;
 
 use gfx::{Device, DeviceHelper};
+use std::vec::Vec;
+use std::option::Option;
+use std::result::Result;
+use std::thread::Thread;
+use std::sync::Arc;
+use std::sync::RwLock;
 
 fn main() {
+    let mut stderr = &mut std::io::stderr();
+    let args = std::os::args_as_bytes();
+    let exe = String::from_utf8_lossy(args[0].as_slice());
+
+    if args.len() != 2 {
+        print_usage(stderr, exe.as_slice());
+        std::os::set_exit_status(1);
+        return;
+    }
+    let input_path = Path::new(args[1].as_slice());
+
+    let waveform = Waveform::new(input_path);
+
     let window = glutin::Window::new().unwrap();
     window.set_title("genesis");
     unsafe { window.make_current() }
@@ -31,7 +53,86 @@ fn main() {
                 _ => {},
             }
         }
+        waveform.read().unwrap().display();
         device.submit(renderer.as_buffer());
         window.swap_buffers();
+    }
+}
+
+fn print_usage(stderr: &mut std::io::LineBufferedWriter<std::io::stdio::StdWriter>, exe: &str) {
+    let _ = write!(stderr, "Usage: {} <file>\n", exe);
+}
+
+enum WaveformLoadState {
+    Error,
+    Spawning,
+    Opening,
+    Reading,
+    Complete,
+}
+
+struct Waveform {
+    buffers: Vec<groove::DecodedBuffer>,
+    load_state: WaveformLoadState,
+}
+
+impl Waveform {
+    fn new(path: Path) -> Arc<RwLock<Self>> {
+        let waveform_arc = Arc::new(RwLock::new(Waveform {
+            load_state: WaveformLoadState::Spawning,
+            buffers: Vec::new(),
+        }));
+        let waveform_rw = waveform_arc.clone();
+        Thread::spawn(move || {
+            let set_load_state = |&: state: WaveformLoadState| {
+                let mut waveform = waveform_rw.write().unwrap();
+                waveform.load_state = state;
+            };
+            set_load_state(WaveformLoadState::Opening);
+            let file = match groove::File::open(&path) {
+                Option::Some(f) => f,
+                Option::None => {
+                    set_load_state(WaveformLoadState::Error);
+                    panic!("unable to open file");
+                },
+            };
+            set_load_state(WaveformLoadState::Reading);
+
+            let playlist = groove::Playlist::new();
+            let sink = groove::Sink::new();
+            sink.set_audio_format(groove::AudioFormat {
+                sample_rate: 44100,
+                channel_layout: groove::ChannelLayout::LayoutStereo,
+                sample_fmt: groove::SampleFormat {
+                    sample_type: groove::SampleType::Dbl,
+                    planar: false,
+                },
+            });
+            match sink.attach(&playlist) {
+                Result::Ok(_) => {},
+                Result::Err(_) => {
+                    set_load_state(WaveformLoadState::Error);
+                    panic!("error attaching sink");
+                }
+            }
+            playlist.append(&file, 1.0, 1.0);
+
+            loop {
+                match sink.buffer_get_blocking() {
+                    Option::Some(decoded_buffer) => {
+                        let mut waveform = waveform_rw.write().unwrap();
+                        waveform.buffers.push(decoded_buffer);
+                    },
+                    Option::None => break,
+                }
+            }
+            set_load_state(WaveformLoadState::Complete);
+            println!("complete");
+        });
+        waveform_arc
+    }
+
+    fn display(&self) {
+        println!("waveform display");
     }
 }
